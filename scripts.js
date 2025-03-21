da0fdb02bc7db55f49ea62237b43e20caa64fe01
Gallardo666 <jordigallardo0621@gmail.com>
2025-03-21 07:57:10 +0100

    Arreglado DarkMode (Falta quitar scroll)

--- scripts.js
+++ scripts.js
@@ -1,23 +1,28 @@
-const body = document.querySelector('body'),
-    sidebar = body.querySelector('.sidebar'), // Ensure this targets the correct element
-    toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body'),
+        sidebar = body.querySelector('.sidebar'), // Ensure this targets the correct element
+        toggle = body.querySelector(".toggle"),
+        searchBtn = body.querySelector(".search-box"),
+        modeSwitch = body.querySelector(".toggle-switch"),
+        modeText = body.querySelector(".mode-text");
 
-toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-});
+    toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    });
 
-searchBtn.addEventListener("click", () => {
-    sidebar.classList.remove("close");
-});
+    searchBtn.addEventListener("click", () => {
+        sidebar.classList.remove("close");
+    });
 
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    if (body.classList.contains("dark")) {
-        modeText.innerText = "Light mode";
-    } else {
-        modeText.innerText = "Dark mode";
-    }
+    modeSwitch.addEventListener("click", () => {
+        if (body.classList.contains('light')) {
+            body.classList.remove('light');
+            body.classList.add('dark');
+            modeText.innerText = "Light mode";
+        } else {
+            body.classList.remove('dark');
+            body.classList.add('light');
+            modeText.innerText = "Dark mode";
+        }
+    });
 });
